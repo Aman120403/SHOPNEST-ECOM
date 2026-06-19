9e9cd6bd92317f38c54927fa49df567b7bf4a9c1
Aman120403 <amanmaurya904463@gmail.com>
2026-06-19 21:19:38 +0530

    Creating controller and model

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(cors());
+app.use(express.json());
 app.get("/", (req,res) =>{
     res.send("Shopnest backend is running");
 });
-
+connectDB();
 app.listen(PORT, () =>{
     console.log(`Server is running on PORT: ${PORT}`);
 });
+
