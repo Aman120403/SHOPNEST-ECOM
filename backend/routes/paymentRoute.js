3c04b0a68dad5ce01dae013c28e79a3bc64eb17b
Aman120403 <amanmaurya904463@gmail.com>
2026-07-04 00:00:13 +0530

    Creating payment route

--- backend/routes/paymentRoute.js
+++ backend/routes/paymentRoute.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const {protect} = require("../middleware/authMiddleware");
 const {admin} = require("../middleware/adminMiddleware");
-const { creatdOrder,verifyPayment } = require("../controller/paymentController");
+const { createdOrder,verifyPayment } = require("../controller/paymentController");
 
 const router = express.Router();
 
-router.post("/order", createOrder);
+router.post("/order", createdOrder);
 router.post("/verify", verifyPayment);
 
 module.exports = router;
