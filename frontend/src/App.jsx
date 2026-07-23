24e1be8e00bc49a2b8c018ccd81d06b843ee56c0
Aman120403 <amanmaurya904463@gmail.com>
2026-07-23 22:31:43 +0530

    Checkout page completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import ProductDetail from './pages/ProductDetail';
 import Cart from './pages/Cart';
-
+import Checkout from './pages/Checkout';
 
 const App = () => {
   return (
@@ -26,6 +26,7 @@ const App = () => {
             <Route path="/register" element={<Register/>} />
             <Route path="/products/:id" element={<ProductDetail/>} />
             <Route path="/cart" element ={<Cart/>} />
+            <Route path="/checkout" element={<Checkout />} />
 
 
         </Routes>
